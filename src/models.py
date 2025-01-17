d2bfa90493ce50412aa916009d177a3159405a38
moyan <1561515308@qq.com>
2025-01-17 22:44:17 +0800

    refactor(betterdict)

--- src/models.py
+++ src/models.py
@@ -3,46 +3,60 @@ import ujson
 
 
 class BetterDict:
+    """
+    更好的字典
 
-    def __init__(self, data: dict) -> None:
-        self.update(data)
+    Attributes:
+        __dict (dict):原始字典
+    """
 
-    def __getitem__(self, key):
-        return self.__dict__[key]
+    def __init__(self, conf: dict):
+        self.__dict = conf
+        self._update()
 
-    def __setitem__(self, key, value):
-        self.__dict__[key] = value
+    def __getattr__(self, name):
+        if name in self.__dict:
+            return self.__dict[name]
+        else:
+            raise AttributeError(f"BetterDict has no attribute '{name}'")
 
-    def __getattr__(self, key):
-        return self.__dict__.get(key)
+    def __setattr__(self, name, value):
 
-    def __setattr__(self, key, value):
-        self.__dict__[key] = value
+        if isinstance(value, dict):
+            value = BetterDict(value)
+        elif isinstance(value, list):
+            value = [
+                BetterDict(item) if isinstance(item, dict) else item for item in value
+            ]
+        self.__dict[name] = value
 
-    def update(self, data: dict):
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-            else:
-                self.__dict__[key] = value
+    def __getitem__(self, name):
+        return self.__getattr__(name)
+
+    def __setitem__(self, name, value):
+        self.__setattr__(name, value)
 
     def __len__(self):
-        return len(self.__dict__)
-
-    def __delitem__(self, key):
-        del self.__dict__[key]
-
-    def __delattr__(self, key):
-        del self.__dict__[key]
-
-    def __iter__(self):
-        return iter(self.__dict__)
+        return len(self.__dict)
 
     def __str__(self):
-        return ujson.dumps(self.__dict__, ensure_ascii=False, indent=4)
+        return ujson.dumps(self.__dict, indent=4, ensure_ascii=False)
 
     def __repr__(self):
-        return f"BetterDict({self.__dict__})"
+        return str(self.__dict)
+
+    def _update(self, conf=None):
+        """
+        更新字典内容
+
+        Args:
+            conf (dict):要更新的字典
+        """
+        if conf:
+            self.__dict = conf
+
+        for key, value in self.__dict.items():
+            self.__setattr__(key, value)
 
     async def __save__(self, path: str):
         async with aiofiles.open(path, "w") as f:

9bda2cc57b60ca065377330dab3c0df841628067
moyan <1561515308@qq.com>
2025-01-19 17:55:08 +0800

    feat(web): 更新首页布局并添加游戏卡片

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import { useConfigStore } from '@/stores';
-import { ElCard } from 'element-plus';
+import { ElCard, ElRow, ElCol } from 'element-plus';
 const configrStore = useConfigStore();
 </script>
 
 <template>
     <h1>你好，{{ configrStore.user_name }}！</h1>
     <br>
-    <el-card>1212</el-card>
+    <el-row :gutter="16">
+        <el-col :span="configrStore.game.ys.enable && configrStore.game.sr.enable ? 12 : 24"
+            v-if="configrStore.game.ys.enable">
+            <h2>原神</h2>
+            <el-card>加载失败。。。</el-card>
+        </el-col>
+        <el-col :span="configrStore.game.ys.enable && configrStore.game.sr.enable ? 12 : 24"
+            v-if="configrStore.game.sr.enable">
+            <h2>崩坏：星穹铁道</h2>
+            <el-card>加载失败。。。</el-card>
+        </el-col>
+    </el-row>
 
 </template>
